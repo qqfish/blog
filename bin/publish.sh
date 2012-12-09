48e1b4aa09b35fcfe935ff1484e9186db90d9ee0
Erwann MEST <erwann.mest@gmail.com>
2012-12-09 15:00:30 +0100

    Source updated

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,13 +1,24 @@
 #!/bin/sh
 grunt
-git co gh-pages
-git merge dev --no-edit
+read -p "Press [Enter] key to continue..."
+git commit -am "Source updated"
+read -p "Press [Enter] key to continue..."
+git checkout gh-pages
+read -p "Press [Enter] key to continue..."
 mv .gitignore .gitignore.tmp
+read -p "Press [Enter] key to continue..."
 git rm -rqf .
+read -p "Press [Enter] key to continue..."
 mv .gitignore.tmp .gitignore
+read -p "Press [Enter] key to continue..."
 cp -r build/ .
+read -p "Press [Enter] key to continue..."
 git add .
+read -p "Press [Enter] key to continue..."
 git rm .gitignore
+read -p "Press [Enter] key to continue..."
 git commit -m "updated"
+read -p "Press [Enter] key to continue..."
 git push -f
-git co dev
+read -p "Press [Enter] key to continue..."
+git checkout dev
