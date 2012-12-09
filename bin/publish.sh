c428d6152f647ff2be386d7f6735562d098b311b
Erwann MEST <erwann.mest@gmail.com>
2012-12-09 15:20:15 +0100

    remove useless code

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,26 +1,14 @@
 #!/bin/sh
 grunt
-read -p "Press [Enter] key to continue..."
 git commit -am "Source updated"
-read -p "Press [Enter] key to continue..."
 git branch -D gh-pages
-read -p "Press [Enter] key to continue..."
 git checkout -b gh-pages
-read -p "Press [Enter] key to continue..."
 mv .gitignore .gitignore.tmp
-read -p "Press [Enter] key to continue..."
 git rm -rqf .
-read -p "Press [Enter] key to continue..."
 mv .gitignore.tmp .gitignore
-read -p "Press [Enter] key to continue..."
 cp -r build/ .
-read -p "Press [Enter] key to continue..."
 git add .
-read -p "Press [Enter] key to continue..."
 git rm .gitignore
-read -p "Press [Enter] key to continue..."
 git commit -m "Site updated"
-read -p "Press [Enter] key to continue..."
 git push -f
-read -p "Press [Enter] key to continue..."
 git checkout dev
